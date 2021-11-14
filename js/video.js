b28b004e0cc171468c617695362cfb9b1523a163
grant <ley5@uw.edu>
2021-11-14 12:13:42 -0500

    add volumne indicator

--- js/video.js
+++ js/video.js
@@ -2,45 +2,56 @@ var video = document.getElementById("player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
 });
 
+function displayPercentage(num) {
+	return num * 100 + "%"
+}
+
 document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
 	video.play();
+	document.querySelector("#volume").textContent = displayPercentage(video.volume);
+
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video")
 	video.pause();
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate = video.playbackRate - 0.05;
+	video.playbackRate = video.playbackRate * .95;
 	console.log("New Speed:" + video.playbackRate);
 
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate = video.playbackRate + 0.05;
+	video.playbackRate = video.playbackRate * 1.05;
 	console.log("New Speed:" + video.playbackRate);
 
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Original location: " + video.currentTime + " sec")
 	let timeLeft = video.duration - video.currentTime;
 	if (timeLeft > 15) {
 		video.currentTime += 15;
 	} else {
 		video.currentTime = 0;
 	}
-	console.log("Current time: " + video.currentTime + " sec")
+	console.log("New location: " + video.currentTime + " sec")
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
 	video.muted = !video.muted;
+	this.textContent = video.muted ? "Unmute" : "Mute";
 });
 
 document.querySelector("#slider").addEventListener("change", function() {
 	video.volume = this.value / 100;
+	console.log(video.volume)
+	document.querySelector("#volume").textContent = displayPercentage(video.volume);
 });
 
 document.querySelector("#vintage").addEventListener("click", function() {
